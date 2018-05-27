a01834e3989e2486a7ba87c7ee6c4cb512545e26
rosavandenende <38073481+rosavandenende@users.noreply.github.com>
2018-05-27 12:18:29 +0200

    Add files via upload

--- untitled34.py
+++ untitled34.py
@@ -18,8 +18,6 @@ from scipy import signal
 from scipy.signal import butter, lfilter
 import glob 
 
-
-
 def butter_lowpass(cutoff, sampling_rate, order=5):
     nyq = 0.5 * sampling_rate
     normal_cutoff = cutoff / nyq
@@ -57,17 +55,16 @@ def butter_bandpass_filter(data, lowcut, highcut, sampling_rate, order=5):
 def PSD_using_scipy(ampl, fs):
     return signal.welch(ampl, fs, nperseg=1024, scaling= 'spectrum')
 
-def main_trigger(n,low_cut,high_cut):
-    file_list = glob.glob("/Volumes/My Passport/Pylos_data/Py1-201650198/*.wav")
+def main_trigger(n,a,b,low_cut,high_cut):
+    file_list = glob.glob("/Volumes/My Passport/Pylos_data/Py1-201650198/*.wav")            #Maakt een lijst met alle bestandsnamen uit het mapje
     time_series = []
     triggered_list = []
     triggered = 0
    
-    for i in file_list[7:8]:
+    for i in file_list[a:b]:
         waveFile = wave.open(i,'r')
         sampling_rate = waveFile.getframerate()
-        length = waveFile.getnframes()
-        for j in range(0,int(n)):
+        for j in range(0,int(n)):                                   #n samples bekijken
             waveFile.setpos(j)
             waveData = waveFile.readframes(1)
             sample_point = struct.unpack("<h", waveData)
@@ -76,32 +73,34 @@ def main_trigger(n,low_cut,high_cut):
 
     time_series_filtered = butter_bandpass_filter(time_series,low_cut,high_cut, sampling_rate,order = 5) 
     
+    
+    samples_per_sigma = 120 * 144000                                                                #Elke 120 seconden wordt sigma opnieuw bepaald
     sigma_list = []
     sigma2_list = []
     teller = 0 
     teller_list = []
-    for i in range(0,int(n)//int(120*144000)):                        #Hoe vaak moet sigma opnieuw bepaald worden --> elke twee minuten
-        sigma = np.std(time_series_filtered[120*144000*i:120*144000*(i+1)])         #Sigma in lijst stoppen
+    for i in range(0,int(n)//int(samples_per_sigma)):                                      
+        sigma = np.std(time_series_filtered[samples_per_sigma*i : samples_per_sigma*(i+1)])         #Bepaling van sigma voor elk stuk van 120 sec       
         sigma_list.append(sigma)
         
-        for element in time_series_filtered[120*144000*i:120*144000*(i+1)]:         #Voor elk element in dit stuk gefilterde lijst: is ie groter dan 5sigma?
-            teller += 1
+        for element in time_series_filtered[samples_per_sigma*i : samples_per_sigma*(i+1)]:         #Voor elk element in dit stuk lijst:  trigger?
+            teller += 1                                                                             #Teller: op welk sample number bevindt de trigger zich 
             if element >= (5*sigma):
                 triggered += 1
                 triggered_list.append(element)
                 teller_list.append(teller)
-       
-    #als niet deelbaar door: 
-    #je moet nog hebben: het stuk van i+1*144000 tot n (einde)
-        
-#    rest = (int(n)//int(144000)+1)*1440000          #Je moet nog het 'restgedeelte' hebben: van (n/144000 + 1)*144000 tot het einde
-#    sigma = np.std(time_series_filtered[-rest:])
-#    sigma_list.append(sigma)
-#    print (sigma_list)
-#    
-    
-    for sigma_value in sigma_list:              #Een lijst maken met de sigmavalues, evenvaak als de samples
-        sigma2_list.extend([sigma_value]*(120*144000))
+
+
+    #als niet deelbaar door samples_per_sigma, dan moet je nog het stuk hebben van k+1 * samples_per_sigma tot het einde, waarbij k de hoogste deler is 
+    rest = n - 
+    rest = n - (int(n)//int(samples_per_sigma)+1)*samples_per_sigma                                                        
+    sigma = np.std(time_series_filtered[-rest:])
+    sigma_list.append(sigma)
+    rest_len = len(time_series_filtered[-rest:])
+  
+    for sigma_value in sigma_list[:-1]:                                                          #Een lijst maken met de sigmavalues, evenvaak als de samples
+        sigma2_list.extend([sigma_value]*(samples_per_sigma))
+    sigma2_list.extend([sigma_list[-1]]*rest_len)
     print (len(sigma_list))
     print (len(sigma2_list))
 
@@ -109,24 +108,21 @@ def main_trigger(n,low_cut,high_cut):
     plt.rcParams["figure.figsize"] = [24,12]
     fig,ax = plt.subplots(2,1,sharex = False)
     ax[0].scatter(teller_list,triggered_list,s=10)
-#    ax1.xlim(-400,len(time_series_filtered))
     ax[0].set_xlabel("sample number")
     ax[0].set_ylabel("ADC")
     ax[0].set_title("Triggered signals, amplitude > 5 * $\sigma$")
     ax[0].text(100000,350, "Number of triggers = %d" %triggered)
     
-    #Offset time_series verwijderen
-#    time_series0 = time_series - np.mean(time_series)
     ax[1].plot(range(len(time_series_filtered)),time_series_filtered,'green',linewidth = 0.7)
-#    plt.plot(range(len(time_series0)),time_series0,linewidth = 0.7)
     ax[1].plot(range(len(time_series_filtered)),sigma2_list,'r--')
-#    ax[1].axhline(y=5*sigma, color='r', linestyle='--')
     ax[1].set_title("Filtered signal (1-40 kHz)")
     ax[1].set_xlabel("sample number")
     ax[1].set_ylabel("ADC")
 
     plt.show()             
  
-main_trigger(10*60*144000,1e3,40e3)
+main_trigger(5*60*144000,1e3,40e3)
+
+#Build in algorithm to maje sure it has only one peak
 
 #Nog even over nadenken: het gefilterde signaal heeft geen offset
