03bacd2365c7dc62a42c2db1f93e75edf67240c5
rosavandenende <38073481+rosavandenende@users.noreply.github.com>
2018-06-12 18:05:34 +0200

    Add files via upload

--- untitled36-aangepast.py
+++ untitled36-aangepast.py
@@ -6,12 +6,17 @@ Created on Wed May 30 14:59:37 2018
 @author: chip
 """
 
+#in this document: plotting the noise level and the number of triggered signals over time. 
+
 import wave, struct
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.signal import butter, lfilter
 import glob 
+from datetime import datetime,timedelta
+import pandas as pd 
+import matplotlib.dates as mdates
 
 def butter_lowpass(cutoff, sampling_rate, order=5):
     nyq = 0.5 * sampling_rate
@@ -39,7 +44,6 @@ def butter_bandpass(lowcut, highcut, sampling_rate, order=5):
     nyq = 0.5 * sampling_rate
     low = lowcut/nyq
     high = highcut/nyq
-    print (low, high)
     b, a = butter(order, [low, high], btype='band', analog=False)
     return b, a
 
@@ -52,68 +56,129 @@ def PSD_using_scipy(ampl, fs):
 
 
 def main_trigger(a,b,c,low_cut,high_cut):
-    file_list = glob.glob("/media/My Passport/Pylos_data/Py1-201650198/*.wav")                    #Maakt een lijst met alle bestandsnamen uit het mapje
+    print (datetime.now())
+    file_list = sorted(glob.glob("/media/My Passport/Pylos_data/Py1-201650198/*.wav"))                    #Maakt een lijst met alle bestandsnamen uit het mapje
     time_series = []
     samples_per_sigma = 120 * 144000                                                                #Elke 120 seconden wordt sigma opnieuw bepaald
     sigma_list = []
-    sigma2_list = []
     triggered = 0
     triggered_list = []
-#    teller = 0 
-#    teller_list = []
-   
+    
+    
+    
+    #Making a list with the time values: 2 minutes are added 
+#    date_list = []
+    dateandtime_list = []
+    dateandtime = datetime(year=2018,month=1,day=29,hour = 9, minute=00,second= 02)
+    deltatime = timedelta(minutes = 2)
+    for i in range(0,c*(b-a)):
+        dateandtime = dateandtime + deltatime
+        dateandtime_list.append(dateandtime)
+    print( "len of datetimelist", len(dateandtime_list))
+    
+    
     for i in file_list[a:b]:
         waveFile = wave.open(i,'r')
         sampling_rate = waveFile.getframerate()      
-
+#       date_list.append(i)
+        
            
         for startpoint in range(0,c):                                                  #hoeveel stukjes van 120 sec wil je
             for j in range(int(startpoint*samples_per_sigma),int((startpoint+1)*samples_per_sigma)):                       #deze is dus max 2 min                                
                 waveFile.setpos(j)
                 waveData = waveFile.readframes(1)
                 sample_point = struct.unpack("<h", waveData)
-                time_series.append(sample_point[0]) 
-                
+                time_series.append(sample_point[0])
             time_series_filtered = butter_bandpass_filter(time_series,low_cut,high_cut, sampling_rate,order = 5) 
             sigma = np.std(time_series_filtered)
             sigma_list.append(5*sigma)
-            print (sigma)    
+            print (sigma,i,j)    
             for element in time_series_filtered:         
-#                teller += 1 
                 if element >= (5*sigma):
-                    triggered += 1
+                   triggered += 1
             triggered_list.append(triggered)
          
                   #Let op!! Hier is triggered_list de lijst met de HOEVEELHEDEN getriggerd, dus niet de getriggerede elementen
-#                    teller_list.append(teller) 
             print (triggered)
             time_series = []
             time_series_filtered = []
             triggered = 0
-                
-    print (len(triggered_list))
-    print (len(sigma_list))
-    print (triggered_list)
-    print (sigma_list)
+    np.savetxt("%i %i %i 5sigma and triggered" %(a,b,c),[sigma_list,triggered_list])
+
+    
+    plt.rcParams["figure.figsize"] = [16,10]
+    fig,ax = plt.subplots(2,1,sharex = True)
+    ax[0].scatter(dateandtime_list,sigma_list,s=10)
+    ax[0].set_xlabel("Date (y-m-d h:m)")
+    ax[0].set_ylabel("Noise level, 5$\sigma$")
+    ax[0].set_title("Noise level over time")
+    plt.subplots_adjust(hspace = 0.4)
+    ax[1].scatter(dateandtime_list,triggered_list,s=10)
+    ax[1].set_title("Number of triggered signals over time")
+    ax[1].set_xlabel("Date (y-m-d h:m)")
+    ax[1].set_ylabel("Number of triggers")
+    xfmt = mdates.DateFormatter("%d-%m-%y %H:%M")
+    ax[0].xaxis.set_major_formatter(xfmt)
+    plt.xticks(rotation = 45)
+    plt.savefig("%i %i %i 5sigma and triggered" %(a,b,c))
+    plt.show()
+    
+    print (datetime.now())
+
+main_trigger(0,5,59,5e3,40e3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    for k in date_list:
+#        date_listshort.append(k[-16:-4])        
+#    print (date_listshort)
+#    for l in date_listshort:
+#        year,month,day,hour,minute,second = l[-16:-14],l[-14:-12],l[-12:-10],l[-10:-8],l[-8:-6],l[-6:-4]
+#        print day
+ #   print (len(triggered_list))
+  #  print (len(sigma_list))
+   # print (triggered_list)
+    #print (sigma_list)
+
 #    for sigma_value in sigma_list[:-1]:                                                               #Een lijst maken met de sigmavalues, evenvaak als de samples
 #        sigma2_list.extend([5*sigma_value]*(samples_per_sigma)) 
 #    sigma2_list.extend([5*sigma_list[-1]]*(rest_len))
      
 #    np.savetxt("30050218_45_3.txt",(triggered_list,sigma_list))
-    plt.rcParams["figure.figsize"] = [16,10]
-    time = np.arange(1,2*c,2)
+ 
+    #time = np.arange(1,2*c*(b-a),2)
+    #time= pd.date_range("29/01/2018 09:00:002",periods = (b-a), freq = "119 min 59 S")
+ 
 
-    fig,ax = plt.subplots(2,1,sharex = True)
-    
-    ax[0].scatter(time,sigma_list)
-    ax[0].set_xlabel("Time (minutes)")
-    ax[0].set_ylabel("Noise level, 5$\sigma$")
-#    ax[0].set_title("Noise level over time, %s" %i[-15])
-    plt.subplots_adjust(hspace = 0.4)
-    ax[1].scatter(time,triggered_list)
-#    ax[1].set_title("Number of triggered signals over time, %s" %i[-15])
-    ax[1].set_xlabel("Time (minutes)")
-    ax[1].set_ylabel("Number of triggers")
-    plt.show()
-
-main_trigger(4,5,20,1e3,40e3)
+#    time_list2 = []
+#    for j in dateandtime_list:
+#        time_list2.extend([j]*59)
